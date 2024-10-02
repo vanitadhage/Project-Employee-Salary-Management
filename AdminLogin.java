e52f0d9522f23a1b57d3ffd91e37698aafd80e6a
vanitadhage <144885494+vanitadhage@users.noreply.github.com>
2024-10-02 22:45:00 +0530

    Create AdminLogin.java

---
+++ AdminLogin.java
@@ -0,0 +1,40 @@
+
+package AdminPanel;
+
+public class AdminLogin
+ { 
+	private int aid;
+	private String username, password;
+
+	public int getAid() 
+	{
+		return aid;
+	}
+
+	public void setAid(int aid) 
+	{
+		this.aid = aid;
+	}
+
+	public String getUsername() 
+	{
+		return username;
+	}
+
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+
+	public String getPassword() 
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+
+ }
